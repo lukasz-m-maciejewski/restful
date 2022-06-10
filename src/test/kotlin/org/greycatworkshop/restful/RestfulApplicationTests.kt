8092a7e6451fa224cb704c4102910c8a0dd48fb9
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2022-06-10 07:50:32 +0200

    initial

---
+++ src/test/kotlin/org/greycatworkshop/restful/RestfulApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.greycatworkshop.restful
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
